cb0c7128f6f4a9c36b8764cf088640763dc4a873
Hanbeen Kim 김한빈 <hanbini2008@naver.com>
2025-07-21 17:30:02 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,36 +1,49 @@
-#include <stdio.h>
+#include <iostream>
 #include <queue>
 using namespace std;
 
-priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > >pq;
-bool visit[100005];
+int n, k;
+int visited[200000] = {0};
+queue<pair<int, int>> q;
 
-int main(){
-	int n,k;
-	
-	scanf("%d %d",&n,&k);
-	
-	pq.push(make_pair(0,n));
-	
-	while(!pq.empty()){
-		int time = pq.top().first;
-		int x = pq.top().second;
-		pq.pop();
+int bfs() {
+	while(!q.empty()) {
+		int value = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
 		
-		visit[x] = 1;
+		if(value == k) return cnt;
 		
-		if(x==k){
-			printf("%d",time);
-			break;
+		int teleport = 2;
+		while(value*teleport < 200000 && visited[value*teleport] == 0) {
+			q.push(make_pair(value*teleport, cnt));
+			visited[value*teleport] = 1;
+			teleport *= 2;
 		}
 		
-		if(x-1>=0 && !visit[x-1])
-			pq.push(make_pair(time+1,x-1));
-		if(x+1<=100000 && !visit[x+1])
-			pq.push(make_pair(time+1,x+1));
-		if(x*2<=100000 && !visit[x*2])
-			pq.push(make_pair(time,x*2));
+		if(visited[value-1] == 0 && value-1 >= 0) {
+			q.push(make_pair(value-1, cnt+1));
+			visited[value-1] = 1;
+		}
+		
+		if(visited[value+1] == 0 && value < 100000) {
+			q.push(make_pair(value+1, cnt+1));
+			visited[value+1] = 1;
+		}
 	}
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
 	
+	cin >> n >> k;
+	
+	q.push(make_pair(n, 0));
+	visited[n] = 1;
+	
+	int res = (n==k) ? 0 : bfs();
+	
+	cout << res;
 	return 0;
 }
