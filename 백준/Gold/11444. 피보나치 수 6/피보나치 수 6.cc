71e1ea7f38194f1020799e7c2ab2d517d74adc2d
Hanbeen Kim 김한빈 <hanbini2008@naver.com>
2025-07-28 12:20:10 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<long, long> m;
+
+#define MOD 1000000007
+long long fib(long long x){
+    if(m[x]) return m[x];
+
+    long long result;
+    if(x % 2 == 0) result = (fib(x/2)*(fib(x/2+1)+fib(x/2-1)))%MOD;
+    else result = ((fib((x+1)/2) * fib((x+1)/2))%MOD)+((fib((x-1)/2)*fib((x-1)/2))%MOD)%MOD;
+
+    return m[x] = result%MOD;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
+    cin >> n;
+    m[0]=0;
+    m[1]=1;
+    m[2]=1;
+    cout << fib(n);
+
+    return 0;
+}
